01fe9aec26d893818805b0b31a1b86b605e86650
babayevhasan <hasanbabayev2005@gmail.com>
2025-05-16 17:41:26 +0400

    ...

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,27 +1,3 @@
-
-// import React from "react";
-// import Header from "../FreightAnnouncements/Header";
-// import StatusChart from "../../components/ui/Modal/Chart/StatusChart";
-// import freightData from "../FreightAnnouncements/freightData.json";
-// import styles from "./Dashboard.module.css";
-
-// const Dashboard = () => {
-//   return (
-//     <>
-//       <Header />
-//       <main className={styles.dashboard}>
-//         <section className={styles.card}>
-//           <h2 className={styles.cardTitle}>Elanların status sayı</h2>
-//           <StatusChart data={freightData} />
-//         </section>
-//       </main>
-//     </>
-//   );
-// };
-
-// export default Dashboard;
-
-
 import React from "react";
 import Header from "../FreightAnnouncements/Header";
 import StatusChart from "../../components/ui/Modal/Chart/StatusChart";
